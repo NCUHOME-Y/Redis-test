368e859f8cc9e0301ae6f905ce637a42a90946f4
TakanashiRikka-Na <2576496262@qq.com>
2023-09-06 17:38:29 +0800

    feat: 清屏

--- client/client.go
+++ client/client.go
@@ -88,6 +88,7 @@ func StartCmd() {
 }
 
 func UseCase() {
+	exec.Command("powershell", "Clear-Host").Run()
 	fmt.Print("SET Key value （将键值存储起来）\nSETNX Key value （如果键存在 返回 0 如果键不存在 返回 1   并存储值)\nGET Key （获得对应键所对应的值）\nDEl Key   （删除所对应的键和值）\nSADD SetName value （向一个集合（SetName）中添加一个元素（value））\nSMEMBER SetName   （获取一个集合内的所有元素）\nLPUSH ListName value （向一个列表（ListName）中添加一个元素（value））\nLRANGE  ListName  start（起始位置） end（结束位置）（获得一个列表 从 start到end 的所有元素）\n 按任意键继续。。。。")
 	fmt.Scanln()
 }
